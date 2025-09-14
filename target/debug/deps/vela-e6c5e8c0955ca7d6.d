0f2f800e541c16d025b32569354ce298a3000cb6
Stormy Wentworth <business@graphicsbystorm.com>
2025-09-14 11:54:19 -0400

    vela-cli: Removed unused rs files.

--- target/debug/deps/vela-e6c5e8c0955ca7d6.d
+++ target/debug/deps/vela-e6c5e8c0955ca7d6.d
@@ -1,6 +1,6 @@
-/home/storm/.vela/cli/target/debug/deps/vela-e6c5e8c0955ca7d6.d: src/main.rs src/cmds/mod.rs src/cmds/shell.rs src/cmds/toggle.rs src/cmds/scheme.rs src/cmds/screenshot.rs src/cmds/record.rs src/cmds/clipboard.rs src/cmds/emoji.rs src/cmds/wallpaper.rs src/cmds/resizer.rs src/cmds/editor.rs src/cmds/install.rs src/cmds/version.rs src/hypr/mod.rs src/util.rs
+/home/storm/.vela/cli/target/debug/deps/vela-e6c5e8c0955ca7d6.d: src/main.rs src/cmds/mod.rs src/cmds/shell.rs src/cmds/toggle.rs src/cmds/scheme.rs src/cmds/screenshot.rs src/cmds/record.rs src/cmds/clipboard.rs src/cmds/emoji.rs src/cmds/wallpaper.rs src/cmds/resizer.rs src/cmds/editor.rs src/cmds/install.rs src/cmds/version.rs src/hypr/mod.rs src/util/mod.rs
 
-/home/storm/.vela/cli/target/debug/deps/libvela-e6c5e8c0955ca7d6.rmeta: src/main.rs src/cmds/mod.rs src/cmds/shell.rs src/cmds/toggle.rs src/cmds/scheme.rs src/cmds/screenshot.rs src/cmds/record.rs src/cmds/clipboard.rs src/cmds/emoji.rs src/cmds/wallpaper.rs src/cmds/resizer.rs src/cmds/editor.rs src/cmds/install.rs src/cmds/version.rs src/hypr/mod.rs src/util.rs
+/home/storm/.vela/cli/target/debug/deps/libvela-e6c5e8c0955ca7d6.rmeta: src/main.rs src/cmds/mod.rs src/cmds/shell.rs src/cmds/toggle.rs src/cmds/scheme.rs src/cmds/screenshot.rs src/cmds/record.rs src/cmds/clipboard.rs src/cmds/emoji.rs src/cmds/wallpaper.rs src/cmds/resizer.rs src/cmds/editor.rs src/cmds/install.rs src/cmds/version.rs src/hypr/mod.rs src/util/mod.rs
 
 src/main.rs:
 src/cmds/mod.rs:
@@ -17,4 +17,4 @@ src/cmds/editor.rs:
 src/cmds/install.rs:
 src/cmds/version.rs:
 src/hypr/mod.rs:
-src/util.rs:
+src/util/mod.rs:
